afdae399ad0d97a722265600dea358ba20e5abb5
davit26g <136718969+davit26g@users.noreply.github.com>
2023-06-24 14:51:34 +0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded',function(){
+    let reload = document.querySelector('.logo');
+
+
+    reload.addEventListener('click',()=>{
+        location.reload();
+    })
+})
