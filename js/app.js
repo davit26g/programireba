c440c6f4785e85eb7789b6c7aa57bcfc994462cd
davit26g <136718969+davit26g@users.noreply.github.com>
2023-07-03 22:03:18 +0400

    dobavlenie

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded',function(){
     let reload = document.querySelector('.logo');
-
+    let showmore = document.querySelector('.seven-li');
 
     reload.addEventListener('click',()=>{
-        location.reload();
+        window.location.href='file:///C:/Users/Davit/Desktop/chtoto/programireba/index.html'
+    });
+
+    showmore.addEventListener('click',()=>{
+        let downblock = document.querySelector('.left-down-block');
+            if(downblock.style.visibility = 'hidden'){
+                downblock.style.visibility = 'visible'
+            }else if(downblock.style.visibility = 'visible'){
+                downblock.style.visibility = 'hidden'
+            }
+        
     })
 })
