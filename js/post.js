a58aa639095187c55799ce684f8467e0b182db17
davit26g <136718969+davit26g@users.noreply.github.com>
2023-08-03 02:55:17 +0400

    this

--- js/post.js
+++ js/post.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded',function(){
     let like = document.querySelector('.like');
     let likeoff = document.querySelector('.likeoff');
-    let likeon = document.querySelector('.likeon');
+    let likeon = document.querySelector('.likeon');    
+
+    /* let content = document.querySelector('.content');
+
+    for(){
+        let posts = document
+    } */
 
     like.addEventListener('click',()=>{
         if(likeoff.style.visibility === ''){
